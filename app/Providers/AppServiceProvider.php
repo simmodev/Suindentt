d73b35b57c5146c8fb66cc31fc666977551905e1
Mohamed Hafidi <MohamedH-a@hotmail.com>
2021-05-02 18:52:05 +0000

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,10 +23,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
-    }
-    
-    if($this->app->environment('production')) {
+        if($this->app->environment('production')) {
         \URL::forceScheme('https');
     }
+    }
+    
+    
 }
